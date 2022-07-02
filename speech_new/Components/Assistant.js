fb3f03a4bdcbca4de0b4a4fbb65f166d92b044fb
dalialotfy <dalia.momen99@eng-st.cu.edu.eg>
2022-07-02 03:48:03 +0300

    ....new

--- speech_new/Components/Assistant.js
+++ speech_new/Components/Assistant.js
@@ -6,11 +6,39 @@ import { useEffect, useState } from 'react';
 // import Dropdown from './Dropdown';
 import Modaal from './Modaal';
 import { ScrollView } from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
+import Icon from 'react-native-vector-icons/FontAwesome';
 // import 'localstorage-polyfill'; 
 export default function Assistant(props) {
   // console.log(props)
+  // ////////////
+  let Ip = '192.168.1.7'
 
+  let pathArray;
+// ########################## local storage for path array ###########################
+  if (localStorage.getItem("pathArrays") === null) {
+    pathArray = [];
+  }
+  else {
+    pathArray = JSON.parse(localStorage.getItem("pathArrays"));
+  }
+  async function playList() {
+    console.log("fe fun esm3", pathArray)
+
+    let data = await fetch(`http://${Ip}:8000/get_list`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        pathArray
+
+
+      })
+    });
+    console.log("fe fun esm3", pathArray)
+  }
+  // /////////////////////////////////////////////
   let [Names, setName] = useState([])
   let names = ["علاء", "اسماعيل", "حسن", "محمد", "احمد", "توفيق"]
   let food = ["لحمة", "فراخ", "ارز", "سمك", "فلفل", "طماطم"]
@@ -22,10 +50,10 @@ export default function Assistant(props) {
   let ArraySent;
   if (localStorage.getItem("ArraySent") === null) {
     ArraySent = [];
-    // console.log("fadyyy")
+
   }
   else {
-    // console.log("msh fadyyy")
+
 
     ArraySent = JSON.parse(localStorage.getItem("ArraySent"));
 
@@ -38,15 +66,6 @@ export default function Assistant(props) {
 
   }
   const pull_data = (data) => {
-    // console.log(data)
-    // let arr=[]
-    // arr.push(data)
-
-    // console.log(arr)
-    //  for(let i=0;i<Names.length;i++)
-    //  {
-    //  arr.map((a,ind)=>{setName(a)})
-    // setName(data)
     console.log(data)
     ArraySent.push(data)
     console.log(ArraySent)
@@ -55,29 +74,24 @@ export default function Assistant(props) {
     setName(ArraySent)
     //  }
 
-    // console.log(Names); // LOGS DATA FROM CHILD (My name is Dean Winchester... &)
-    // Names.map((name,index)=>{console.log(name)})
-
   }
 
-  function clear() {
-    // setModalVisible(!modalVisible);
-    // pathArray=null
-    localStorage.clear()
-    if (localStorage.getItem("ArraySent") === null) {
-      ArraySent = [];
-      // console.log("fadyyy")
-    }
-    else {
-      // console.log("msh fadyyy")
+  // function clear() {
 
-      ArraySent = JSON.parse(localStorage.getItem("ArraySent"))
+  //   localStorage.clear()
+  //   if (localStorage.getItem("ArraySent") === null) {
+  //     ArraySent = [];
+ 
+  //   }
+  //   else {
+ 
 
-    }
-    // console.log(pathArray)
-    // console.log("clear")
+  //     ArraySent = JSON.parse(localStorage.getItem("ArraySent"))
 
-  }
+  //   }
+
+
+  // }
 
   let categs = ['أشخاص', 'أفعال', 'حروف_الجر', 'ملابس', 'طعام', 'أجهزة_كهربائية', 'غرف_النوم', 'مطبخ', 'غرفة_المعيشة', 'ألوان', 'أسامي_الغرف', 'أدوات_مدرسية']
   let imgs = [require(`../assets/categ/person.jpg`), require(`../assets/categ/action2.png`), require(`../assets/categ/word.png`), require(`../assets/categ/cloth.png`), require(`../assets/categ/food.jpg`), require(`../assets/categ/device.png`), require(`../assets/categ/room.jpg`),require(`../assets/categ/kitchen.png`), require(`../assets/categ/live.png`), require(`../assets/categ/color.png`), require(`../assets/categ/rooms.png`), require(`../assets/categ/school.png`)]
@@ -87,15 +101,13 @@ export default function Assistant(props) {
         <View style={{ flex: 1, backgroundColor: '#251e51', alignItems: 'center', justifyContent: 'center' }}>
           <Text style={styles.buttonTextStyle}>كون جملتك من التصنيفات التالية</Text>
           <View style={styles.spaceText}>
-
-          <Icon onPress={clear} name='delete' size={30} color="white" />
               {ArraySent.map((name, index) =>
                 <Text key={index} style={styles.sentence}>{name}</Text>
               )
-              }
-
+          }
+          <Icon onPress={playList} name='assistive-listening-systems' size={30} color="white" />
           </View>
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',       flexDirection:Platform.OS === ('android')?'column':Platform.OS === ('ios')?'column':'row', flexWrap: 'wrap', alignContent: 'space-around', width: '80%' }}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',flexDirection:Platform.OS === ('android')?'column':Platform.OS === ('ios')?'column':'row', flexWrap: 'wrap', alignContent: 'space-around', width: '80%' }}>
 
             <Modaal categ={categs[0]} imag={imgs[0]} func={pull_data} />
             <Modaal categ={categs[1]} imag={imgs[1]} func={pull_data} />
@@ -178,12 +190,9 @@ const styles = StyleSheet.create({
  },
  sentence:
  {
-    width: 350,
-    height: 40,
-    marginEnd: 5,
-    textAlign: 'center',
+
     color: '#FFFFFF',
-    paddingVertical: 10,
+    paddingVertical: 8,
     fontSize: 18,
     fontWeight: 'bold',
 
@@ -214,36 +223,12 @@ const styles = StyleSheet.create({
 
   space:
   {
-    // display:'flex',
-    // justifyContent:'center',
-    // alignItems:'center',
+
     width: 200,
     height: 40,
-    // margin:'auto'        
+     
   },
 
-  // square:
-  // {
-  //   width: 1100,
-  //   height: 700,
 
-  //   backgroundColor: 'rgba(173, 216, 230,0.5)',
-  //   display: 'flex',
-  //   justifyContent: 'flex-start',
-  //   alignItems: 'center'
-
-  // },
-
-  // div:
-  // {
-  //   width: 700,
-  //   height: 450,
-  //   margin: 'auto',
-  //   backgroundColor: 'rgb(0, 0, 128)',
-  //   display: 'flex',
-  //   justifyContent: 'center',
-  //   alignItems: 'center'
-
-  // }
 
 });
--- speech_new/Components/Modaal.js
+++ speech_new/Components/Modaal.js
@@ -1,104 +1,60 @@
 import React, { useState } from "react";
 import { Alert, Modal, StyleSheet, Text, Pressable, View, TouchableOpacity, Platform, Image } from "react-native";
-// import { TouchableWithoutFeedback } from "react-native-gesture-handler";
-// import { clickProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 import Icon from 'react-native-vector-icons/AntDesign';
 import { ScrollView } from "react-native";
 // import 'localstorage-polyfill'; 
 
 
 const Modaal = (props) => {
+  let Ip = '192.168.1.7'
+
   const [modalVisible, setModalVisible] = useState(false);
   let categs = ['أشخاص', 'أفعال', 'حروف_الجر', 'ملابس', 'طعام', 'أجهزة_كهربائية', 'غرف_النوم', 'مطبخ', 'غرفة_المعيشة', 'ألوان', 'أسامي_الغرف', 'أدوات_مدرسية']
-  //fun fetch w ab3t fel api query{categs[i]} w a7ot elfun de fe click() w a3ml equal lel result m3 detail
   let [detail, setDetail] = useState([])
   let [path, setPath] = useState('')
   let count = 0
   let pathArray;
-  let [Name, setName] = useState("")
-
+  let [Name, setName] = useState([])
+// ########################## local storage for path array ###########################
   if (localStorage.getItem("pathArrays") === null) {
     pathArray = [];
-    // console.log("fadyyy")
   }
   else {
-    // console.log("msh fadyyy")
-
     pathArray = JSON.parse(localStorage.getItem("pathArrays"));
-
   }
-  let Ip = '192.168.1.2'
   async function click() {
     setModalVisible(true)
     for (let c = 0; c < 13; c++) {
       if (props.categ == categs[c]) {
-        //  console.log(categs[c])
         let response = await fetch(`http://${Ip}:8000/category/?table=${categs[c]}`)
         let finalResponse = await response.json()
         console.log(finalResponse.Names)
-        //  finalResponse.Names.map((Names)=>{console.log(Names)})
-        //  let categName=(finalResponse.Names).map(name=>name)
-        // detail=categName.join("\n")
         setDetail(finalResponse.Names)
         setPath(finalResponse.Paths)
         count = finalResponse.Names.length
-        // for(let i=0 ; i<(finalResponse.Names).length;i++)
-        // {
-        //   console.log(finalResponse.Names[i])
-
-
-
-        // }
-
-
       }
     }
   }
   async function listen_db(id, Name) {
-
-
     console.log(id)
-    //setModalVisible(true)
+
     for (let c = 0; c < 12; c++) {
 
       if (props.categ == categs[c]) {
         let response = await fetch(`http://${Ip}:8000/findname/?table=${categs[c]}&id=${id}`)
         let finalResponse = await response.json()
         let path = finalResponse.path
-        // console.log("Before push again",pathArray)
-        // pathArray.push(path)
-        // localStorage.setItem("pathArrays",JSON.stringify(pathArray) )
-        // console.log(path)
-        // console.log(pathArray)
-        //da byktb el gomla
-        // let obj = { "id": id, "table": categs[c] }
-        // pathArray.push(obj)
-        // localStorage.setItem("pathArrays", JSON.stringify(pathArray))
-        // // console.log(obj)
-        // console.log("in listendb", pathArray)
 
       }
     }
-    // setName(Name)
-    // console.log(Name)
-    // props.func(Name);
   }
   async function writeSent(id,Name)
   {
     console.log(id)
-    //setModalVisible(true)
     for (let c = 0; c < 12; c++) {
 
       if (props.categ == categs[c]) {
         let response = await fetch(`http://${Ip}:8000/findname/?table=${categs[c]}&id=${id}`)
-        // let finalResponse = await response.json()
-        // let path = finalResponse.path
-        // console.log("Before push again",pathArray)
-        // pathArray.push(path)
-        // localStorage.setItem("pathArrays",JSON.stringify(pathArray) )
-        // console.log(path)
-        // console.log(pathArray)
-        //da byktb el gomla
         let obj = { "id": id, "table": categs[c] }
         pathArray.push(obj)
         localStorage.setItem("pathArrays", JSON.stringify(pathArray))
@@ -112,60 +68,49 @@ const Modaal = (props) => {
     props.func(Name);
   }
 
-  async function playList() {
-    console.log("fe fun esm3", pathArray)
+  // async function playList() {
+  //   console.log("fe fun esm3", pathArray)
 
-    let data = await fetch(`http://${Ip}:8000/get_list`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        pathArray
+  //   let data = await fetch(`http://${Ip}:8000/get_list`, {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       pathArray
 
 
-      })
-    });
-    console.log("fe fun esm3", pathArray)
-  }
-  //   function listen(name,idx)
-  //  {
-  //    listen_db(idx+1)
-  //    setName(name)
-  //    console.log(name)
-  //    props.func(name);
-
-  //  }
-
-  // function delete_One(index)
-  // {
-  //   // function check(){
-  //   //      return index
-  //   // }
-  //   // pathArray.find(check())
-  //   pathArray.splice(index,1)
-  //   console.log(index)
-  //   console.log(pathArray)
-  //   console.log("delete one")
+  //     })
+  //   });
+  //   console.log("fe fun esm3", pathArray)
   // }
-  function clear() {
-    // setModalVisible(!modalVisible);
-    // pathArray=null
-    localStorage.clear()
-    if (localStorage.getItem("pathArrays") === null) {
+
+  // ##################################  Clear word  ###########################################
+  function clear(name) 
+  {    
+    // setName(array)
+
+    let array= JSON.parse(localStorage.getItem("ArraySent"))
+    let indx=array.indexOf(name)
+    console.log("array",array)
+    console.log("indx",indx)
+    console.log("name",name)
+    if (indx!=-1)
+    {
+      array.splice(indx,1)
+
+    }
+    localStorage.setItem("ArraySent",JSON.stringify(array))
+    if (localStorage.getItem("ArraySent") === null) {
       pathArray = [];
-      // console.log("fadyyy")
     }
     else {
-      // console.log("msh fadyyy")
-
-      pathArray = JSON.parse(localStorage.getItem("pathArrays"))
+      pathArray = JSON.parse(localStorage.getItem("ArraySent"))
 
     }
-    // console.log(pathArray)
-    // console.log("clear")
-
+    // console.log(Name)
+    // props.func(array);
   }
   return (
     <View style={styles.centeredView}>
@@ -181,7 +126,7 @@ const Modaal = (props) => {
       >
 <ScrollView>
         <View style={styles.centereddView}>
-          {/* <Text>{Name}</Text> */}
+    
 
           <View style={styles.modalView}>
           <Icon name='closecircle' size={30} color='white' style={styles.buttonClose} onPress={() => setModalVisible(!modalVisible)}/>
@@ -189,37 +134,13 @@ const Modaal = (props) => {
             {detail.map((name, index) =>
              <Pressable  style={styles.detail}  key={index}>
               <Text  key={index} style={styles.buttonTextStyle}>
-                {/* <Icon onPress={() => { delete_One(index) }} name='delete' size={30} color="white" /> */}
                 {name}
-
-
-                <Icon  style={{marginRight:10}} name="delete" size={30} color='white'/>
+                <Icon  style={{marginRight:10}} onPress={()=>clear(name)} name="delete" size={30} color='white'/>
                 <Icon style={{marginRight:10}} onPress={()=>writeSent(index+1,name)} name='pluscircle' size={30} color='white' />
                 <Icon  style={{marginRight:250}} onPress={() => listen_db(index+1,name)} name="play" size={30} color='white'/>        
                 </Text>
             </Pressable>)}
-            {/* <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => playList()}
-            >
-              <Text style={styles.TextStyle} >اسمع ما اخترته</Text>
-            </Pressable> */}
-            {/* <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => clear()}
-            >
-              <Text style={styles.TextStyle}>الانتقال الى جملة جديدة</Text>
-            </Pressable> */}
-            {/* <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
-            >
-              <Text style={styles.TextStyle}>إغلاق</Text>
-            </Pressable> */}
-
-
           </View>
-
         </View></ScrollView>
 
       </Modal>
@@ -300,7 +221,6 @@ const styles = StyleSheet.create({
     width: '40%'
   },
   detail:{
-    // flex:1,
     flexDirection:'column',
     flexWrap:'wrap',
     alignContent:'space-around',
@@ -310,15 +230,12 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     fontSize: 30,
     fontWeight: 'bold',
-    // justifyContent: 'center',
-    // textAlign: 'center',
     marginBottom: 20,
     marginTop: 10
 
   },
   buttonTextStyle: {
     color: '#FFFFFF',
-    //   paddingVertical: 10,
     fontSize: 30,
     fontWeight: 'bold',
     justifyContent: 'center',
@@ -336,7 +253,6 @@ const styles = StyleSheet.create({
   TextStyle:
   {
     color: '#FFFFFF',
-    //   paddingVertical: 10,
     fontSize: 28,
     fontWeight: 'bold',
     justifyContent: 'center',
@@ -352,40 +268,6 @@ const styles = StyleSheet.create({
 
 
   },
-
-  // textStyle: {
-  //   color: "white",
-  //   fontWeight: "bold",
-  //   textAlign: "center"
-  // },
-  // modalText: {
-  //   marginBottom: 15,
-  //   textAlign: "center"
-  // },
-  // buttonStyle: {
-  //   backgroundColor: '#622da4',
-  //   borderWidth: 0,
-  //   color: '#FFFFFF',
-  //   borderColor: '#7DE24E',
-  //   height: 40,
-  //   alignItems: 'center',
-  //   borderRadius: 30,
-  //   // marginLeft: 35,
-  //   // marginRight: 35,
-  //   marginTop: 50,
-  //   marginBottom: 50,
-  //   // margin:'auto',
-  //   justifyContent:'center',
-  //   width:'400%',
-
-  // },
-  // buttonTextStyle: {
-  //   color: '#FFFFFF',
-  //   paddingVertical: 10,
-  //   fontSize: 18,
-  //   fontWeight:'bold',
-  //   padding:25
-  // },
 });
 
 export default Modaal;
